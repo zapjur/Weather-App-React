b02adf4919b422e8e6b4cbc99dfa13a3261b1a09
Piotr Zapiór <piotrzapior0@gmail.com>
2024-03-22 00:34:05 +0100

    brzydkie ale działa

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'c64700c362msh17aa7cfb5fa5009p177590jsnf8a39199727c',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3026d1a148dd5ae318a22a1e06678813";
